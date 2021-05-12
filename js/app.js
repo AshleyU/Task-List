91cb6c70519b667b2a709ada88ab672bcc731bb4
Ashley U <ashley.umetsu@gmail.com>
2021-05-12 12:55:26 -0500

    Fixed Status selector

---
+++ js/app.js
@@ -0,0 +1,16 @@
+//Implement a JavaScript function to validate your form fields
+
+const newTaskNameInput = document.querySelector('#newTaskNameInput');
+const newTaskDescription = document.querySelector('#newTaskDescription');
+const assignedTo = document.querySelector('#assignedTo');
+const dueDate = document.querySelector('#dueDate');
+const selectedStatus = document.querySelector('#selectedStatus');
+const taskName = newTaskNameInput.value;
+const taskDescription = newTaskDescription.value;
+const assigned = assignedTo.value;
+const date = dueDate.value;
+const status = selectedStatus.value;
+
+const validFormFieldInput = (data) => {
+    
+}
