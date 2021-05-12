b8de44d22638c22c965ea9fb3806ef69f8e44981
Ashley U <ashley.umetsu@gmail.com>
2021-05-12 14:51:53 -0500

    adding alert toggle

--- js/app.js
+++ js/app.js
@@ -1,17 +1,30 @@
 // //Implement a JavaScript function to validate your form fields
-
-const task = document.querySelector('#newTaskNameInput').value;
-const description = document.querySelector('#newTaskDescription').value;
-const assignedTo = document.querySelector('#assignedTo').value;
-const dueDate = document.querySelector('#dueDate').value;
-const selectedStatus = document.querySelector('#selectedStatus').value;
-
-console.log(task);
+const submitButton = document.getElementById('submitButton');
 
 
-// const validFormFieldInput = (data) => {
-//     console.log(task);
-//     if (task == "") {
-//         return task;
-//     };
-// }
+const validFormFieldInput = (data) => {
+    const task = document.querySelector('#newTaskNameInput').value;
+    const description = document.querySelector('#newTaskDescription').value;
+    const assignedTo = document.querySelector('#assignedTo').value;
+    const dueDate = document.querySelector('#dueDate').value;
+    const selectedStatus = document.querySelector('#selectedStatus').value;
+}
+
+//Changed submit button to normal button type and adding event listener
+submitButton.addEventListener('click', validFormFieldInput);
+
+function toggleAlert() {
+    let alert = document.getElementById('alert');
+    let alertDisplay = alert.style.display;
+
+    if (alertDisplay == 'block') {
+        alert.style.display = 'none';
+    }
+    else {
+        alert.style.display = 'block';
+        alert.innerHTML = 'test';
+    }
+ }
+
+//add an alery pop up after submitting if there is an error in the form
+submitButton.addEventListener('click', toggleAlert);
